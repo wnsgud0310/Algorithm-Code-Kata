f0a8c9525510fa3c42359ab7c118aa8bef838f3f
wnsgud0310 <101302585+wnsgud0310@users.noreply.github.com>
2024-09-07 22:04:39 +0900

    [Bronze V] Title: 사분면 고르기, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -1,11 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-   
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+
         if(A >0  && B>0) {
             System.out.println("1");
         } else if (A<0 && B>0) {
