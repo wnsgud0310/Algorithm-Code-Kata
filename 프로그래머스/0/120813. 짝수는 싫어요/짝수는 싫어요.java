7861cec95477c915987b60795f4ee5ccd516186e
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-05-08 15:32:49 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,13 +1,12 @@
 class Solution {
     public int[] solution(int n) {
-        int[] answer = new int[(n+1)/2];
-        
-        for(int i = 1; i <= n; i++) {
-            if(i % 2 == 1) {
-                answer[i/2] = i;
-            }
+
+        int[] answer = new int[(n+1)/2]; 
+     
+        for(int i = 1; i <= n; i++){     
+            if(i % 2 == 1) // 홀수일 때
+                answer[i/2] += i;
         }
-        
         return answer;
     }
 }
