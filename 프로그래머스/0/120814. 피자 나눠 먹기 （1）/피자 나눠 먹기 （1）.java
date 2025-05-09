33cea474ff989ae2ab84afe132084f7117880933
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-05-09 13:32:26 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,12 +1,6 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        if(n%7==0){
-                answer = n/7;
-        }else
-                answer =n/7 +1;
-
-
+        int answer = (n % 7 == 0) ? n / 7 : n / 7 + 1;
         return answer;
     }
 }
