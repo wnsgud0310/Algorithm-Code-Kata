a01b69b24da38678e327c43133dbb1c63c49651c
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-05-18 19:05:16 +0900

    [Bronze V] Title: ??!, Time: 180 ms, Memory: 17784 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class Main{
-    public static void main(String[] args){
+    public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        System.out.println(input + "??!");
+        System.out.println(input+"??!");
     }
 }
