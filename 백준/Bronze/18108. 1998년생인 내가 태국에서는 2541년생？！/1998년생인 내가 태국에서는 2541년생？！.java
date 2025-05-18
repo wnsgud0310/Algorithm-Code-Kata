a24df4dfdbe9e04fcb25aa2cae7c034e031a7783
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-05-18 19:44:04 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 172 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String args[]){
+       Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
-        System.out.println( A - 543);
+        System.out.println(A-543);
     }
 }
