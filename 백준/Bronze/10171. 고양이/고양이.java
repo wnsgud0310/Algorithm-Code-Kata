05a8620482edfc4d5e4fe97fc9a2b0d7118dd108
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-05-21 14:54:40 +0900

    [Bronze V] Title: 고양이, Time: 104 ms, Memory: 13916 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,10 +1,10 @@
+
 public class Main {
-	public static void main(String[] args) {
-    
-		System.out.println("\\    /\\");
-		System.out.println(" )  ( ')");
-		System.out.println("(  /  )");
-		System.out.println(" \\(__)|");    
- 
-	}
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
 }
