e0956d9b274994ad22c02e63ff17645614418f3b
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-05-26 15:05:33 +0900

    [Bronze IV] Title: 주사위 세개, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -2,56 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int prize = 0;
 
-        int a, b, c;
-        a = in.nextInt();
-        b = in.nextInt();
-        c = in.nextInt();
+        if (A == B && B == C) { // 같은 눈 3개
+            prize = 10000 + A * 1000;
+        } else if (A == B || A == C) { // 같은 눈 2개 (A와 B 또는 A와 C)
+            prize = 1000 + A * 100;
+        } else if (B == C) { // 같은 눈 2개 (B와 C)
+            prize = 1000 + B * 100;
+        } else { // 모두 다른 눈
+            int max = Math.max(A, Math.max(B, C));
+            prize = max * 100;
+        }
 
-        // 만약 모든 변수가 다른 경우
-        if (a != b && b != c && a != c) {
-            int max;
-            // 만약 a > b 라면
-            if (a > b) {
-                // c > a > b 라면
-                if (c > a) {
-                    max = c;
-                }
-                // a > (b, c)
-                else {
-                    max = a;
-                }
-            }
-            // b > a 라면
-            else {
-                // c > b > a 라면
-                if (c > b) {
-                    max = c;
-                }
-                // b > (a, c)
-                else {
-                    max = b;
-                }
-            }
-            System.out.println(max * 100);
-        }
-        // 적어도 한 쌍 이상의 서로 같은 변수가 존재할 경우
-        else {
-            // 3개의 변수가 모두 같은 경우
-            if (a == b && a == c) {
-                System.out.println(10000 + a * 1000);
-            }
-            else {
-                // a가 b혹은 c랑만 같은 경우
-                if(a == b || a == c) {
-                    System.out.println(1000 + a * 100);
-                }
-                // b가 c랑 같은 경우
-                else {
-                    System.out.println(1000 + b * 100);
-                }
-            }
-        }
+        System.out.println(prize);
     }
 }
