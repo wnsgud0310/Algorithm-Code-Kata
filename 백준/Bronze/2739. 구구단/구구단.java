a7006f8e0823c286b6ada937fda67de0984eeb70
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-05-27 15:53:52 +0900

    [Bronze V] Title: 구구단, Time: 244 ms, Memory: 18536 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,14 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-
-        sc.close();
-
-        for (int i = 1; i <= 9; i++) {
-            System.out.println(a + " * " + i + " = " + (a * i));
+        int N = sc.nextInt();
+        for(int i = 1; i <= 9; i++) {
+            System.out.println(N + " * " + i + " = " + (N * i));
         }
     }
 }
