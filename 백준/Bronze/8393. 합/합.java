0d614c37dd70811dea850af4e5d818031d21b930
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-05-29 15:11:23 +0900

    [Bronze V] Title: 합, Time: 172 ms, Memory: 17628 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int sum = 0;
-
-        for (int i = 1; i <= N; i++) {
-            sum += i;
+        int sum =0;
+        //1부터 N까지의합
+        for(int i =1; i<=N; i++){
+            sum +=i;
         }
         System.out.println(sum);
-
     }
 }
