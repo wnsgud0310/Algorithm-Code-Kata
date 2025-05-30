38ccdf587c833e1825adfbc2e54abe7de2b6c0f4
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-05-30 16:16:20 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 176 ms, Memory: 17568 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
-
-public class Main {
-
-	public static void main(String[] args) {
-
-		Scanner scan = new Scanner(System.in);
-		
-		int n = scan.nextInt();
-		String str = "";
-		scan.close();
-		
-		for(int i=1; i<=n/4; i++) {
-			str += "long ";
-		}
-		System.out.println(str + "int");
-
-	}
-}
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        int count = N / 4;
+        
+        for(int i = 0; i <count; i++){
+            System.out.print("long ");
+        }
+        System.out.println("int");
+       
+        }
+    }
