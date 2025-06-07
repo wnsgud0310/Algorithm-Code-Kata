cc9963e5d2e526d9dc11a0bcf45ad006715d9cf6
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-06-07 14:25:56 +0900

    [Bronze III] Title: 최소, 최대, Time: 1512 ms, Memory: 311764 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,22 +1,20 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt(); // 정수의 개수
-        int[] arr = new int[N];
+        int N = sc.nextInt();
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
 
         for(int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+            int val = sc.nextInt();
+            if(val < min) min = val; 
+            if(val > max) max = val;
         }
 
-        Arrays.sort(arr); // 오름차순 정렬
-
-        // 정렬했으므로 첫 번째 값이 최솟값, 마지막 값이 최댓값
-        System.out.println(arr[0] + " " + arr[N - 1]);
-
+        System.out.println(min + " " + max);
         sc.close();
     }
 }
