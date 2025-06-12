88b4c61ea99eea2db3f56373349300bd7db8e939
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-06-12 17:02:44 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.05 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,21 +1,10 @@
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 class Solution {
     public int solution(int n) {
-        List<Integer> numList = IntStream.range(1, 1001)
-                                         .map(num -> num * num)
-                                         .boxed()
-                                         .collect(Collectors.toList());
-        Iterator<Integer> iterator = numList.iterator();
-
-        while (iterator.hasNext()) {
-            if (n == iterator.next()) {
-                return 1; // n이 제곱수인 경우 1을 반환
-            }
+        int sqrt = (int)Math.sqrt(n); 
+        if(sqrt * sqrt ==n ){
+            return 1;
+        }else{
+            return 2;
         }
-        return 2; // n이 제곱수가 아닌 경우 2를 반환
     }
 }
