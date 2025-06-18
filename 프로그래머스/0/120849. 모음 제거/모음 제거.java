baee27595fa89275933b79d090857d115e4e9db5
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-06-18 15:02:45 +0900

    [level 0] Title: 모음 제거, Time: 0.11 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,5 +1,7 @@
 class Solution {
     public String solution(String my_string) {
-        return my_string.replaceAll("[aeiou]","");
+        
+        return my_string.replaceAll("[a,e,i,o,u]", "");
+        
     }
 }
