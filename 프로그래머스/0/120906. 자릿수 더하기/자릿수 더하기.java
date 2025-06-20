ef133e50b6aa1c7ca9514e32bf2fbb155bdadbfd
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-06-20 13:36:33 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,9 +1,10 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        
         while(n>0){
-            answer +=n%10;
-            n/=10;
+            answer += n%10; // 마지막자리 더함
+            n = n/10; // 마지막자리 제거
         }
         return answer;
     }
