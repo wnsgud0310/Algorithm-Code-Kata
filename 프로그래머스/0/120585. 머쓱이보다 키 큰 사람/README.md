2baccb8f55baf3ec0beb2cfbe8a4d7e6d272db14
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-06-24 05:35:55 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/README.md
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/README.md
@@ -1,10 +1,10 @@
 # [level 0] 머쓱이보다 키 큰 사람 - 120585 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120585?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120585) 
 
 ### 성능 요약
 
-메모리: 74 MB, 시간: 0.03 ms
+메모리: 88.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 27일 00:41:52
+2025년 06월 24일 05:35:53
 
 ### 문제 설명
 
--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -1,10 +1,12 @@
 class Solution {
     public int solution(int[] array, int height) {
         int answer = 0;
-        for(int i =0; i<array.length; i++){
-            if(array[i]>height)
-            answer ++;
+        for(int i =0; i < array.length; i++){
+            if(array[i] > height){
+                answer++;
+            }
         }
+        
         
         return answer;
     }
