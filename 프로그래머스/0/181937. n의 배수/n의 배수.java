fa122cadf6a224bc7a3b465ece474d7a91630663
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-07-07 17:38:33 +0900

    [level 0] Title: n의 배수, Time: 0.02 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/0/181937. n의 배수/n의 배수.java
+++ 프로그래머스/0/181937. n의 배수/n의 배수.java
@@ -1,12 +1,5 @@
 class Solution {
     public int solution(int num, int n) {
-        
-        if(num%n==0){
-            return 1;
-        }
-        else{
-            return 0;
-        }
-        
+        return (num % n == 0) ? 1 : 0;
     }
 }
