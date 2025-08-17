c7499c9454343d0de6120c6b10c80b561665c321
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-08-17 23:52:52 +0900

    [level 0] Title: 가위 바위 보, Time: 4.87 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,22 +1,12 @@
-import java.lang.StringBuilder;
-
 class Solution {
     public String solution(String rsp) {
-        //  가위 바위 보를 내는데 이길려면
-        // 바위, 보, 가위가 순서대로 나와야함
-        // 0,5,2 가 순서대로 나오게
-        StringBuilder sb = new StringBuilder();
-        for(String str : rsp.split("")){
-            if(str.equals("2")){
-                sb.append("0");
-            }else if(str.equals("0")){
-                sb.append("5");
-            }else {
-                sb.append("2");
-            }
+        String answer = "";
+        String[] arr = rsp.split(""); 
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i].equals("2")) answer += "0";
+            else if (arr[i].equals("0")) answer += "5";
+            else answer += "2";
         }
-        String answer = sb.toString();
         return answer;
     }
-
 }
