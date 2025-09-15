66965c6d6145d762f9a14508b14a025a7786c59f
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-09-15 15:46:49 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 3.55 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,15 +1,19 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] numbers) {
+        // 1. 배열 정렬
         Arrays.sort(numbers);
-        
-        int a = numbers[0]*numbers[1];
-        int b = numbers[numbers.length-1] * numbers[numbers.length-2];
-        
-        if(a>b){
-            return a;
-        }else
-            return b;
+
+        int n = numbers.length;
+
+        // 2. 가장 큰 양수 2개 곱
+        int max1 = numbers[n - 1] * numbers[n - 2];
+
+        // 3. 가장 작은 음수 2개 곱
+        int max2 = numbers[0] * numbers[1];
+
+        // 4. 둘 중 큰 값 반환
+        return Math.max(max1, max2);
     }
 }
