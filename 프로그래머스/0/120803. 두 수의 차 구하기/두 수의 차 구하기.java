ec664c1cd6fc42d2bf83e77b779eebbfae445815
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-09-16 10:11:21 +0900

    [level 0] Title: 두 수의 차 구하기, Time: 0.02 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차 구하기/두 수의 차 구하기.java
+++ 프로그래머스/0/120803. 두 수의 차 구하기/두 수의 차 구하기.java
@@ -1,7 +1,8 @@
 class Solution {
     public int solution(int num1, int num2) {
         int answer = 0;
-        answer = num1-num2;
+        answer = num1 - num2;
+        
         return answer;
     }
 }
