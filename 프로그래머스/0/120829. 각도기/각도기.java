8ad9480f330be9bd38c3e66f427bf79e6b4d22c6
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-09-16 10:48:07 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.java
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -1,15 +1,15 @@
 class Solution {
     public int solution(int angle) {
         int answer = 0;
-        if (0 < angle && angle < 90) {
-            answer = 1; // 예각
-        } else if (angle == 90) {
-            answer = 2; // 직각
-        } else if (90 < angle && angle < 180) {
-            answer = 3; // 둔각
-        } else if (angle == 180) {
-            answer = 4; // 평각
-        }
+        if(angle>0 && angle<90){
+            answer = 1;
+        }else if(angle == 90){
+            answer = 2;
+        }else if(angle>90 && angle<180){
+            answer = 3;
+        }else if(angle == 180) {
+            answer = 4;
+        }    
         return answer;
     }
 }
