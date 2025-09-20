e0531aebee54b7f0a54b7e8916cdd1cd5d613d03
박준형 <101302585+wnsgud0310@users.noreply.github.com>
2025-09-20 17:31:20 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.06 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -1,21 +1,22 @@
 class Solution {
     public int[] solution(long n) {
-        // Long 값을 문자열로 변환
+        
+        // Long 값을 문자열로 변환 
         String s = Long.toString(n);
         
-        // 문자열 뒤집기
+        // 문자열 뒤집기 
         String rs = new StringBuilder(s).reverse().toString();
         
-        // 뒤집힌 문자열 길이 만큼 배열 준비
+        // 3) 뒤집힌 문자열 길이만큼 int 배열 준비
         int[] answer = new int[rs.length()];
         
-        // 각 문자를 숫자로 변환해 배열에 채운다.
-        // 문자를 숫자로 바꾸는법 '0'을 빼면 실제 정수값이 된다(아스키코드)
+        // 4) 각 문자(char)를 숫자(int)로 변환해 배열에 채운다
+        //    문자를 숫자로 바꾸는 표준 트릭: '0'을 빼면 실제 정수값이 된다
         for(int i = 0; i<rs.length(); i++){
-            answer[i] = rs.charAt(i)-'0';
+            answer[i] = rs.charAt(i) - '0';
         }
-
-  
+          
+            
         return answer;
     }
 }
